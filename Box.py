40feedf18d437726c2fc78d687481a6eb92a4982
matanew1 <matanew1@gmail.com>
2023-03-05 12:20:49 +0200

    changed board and box files

--- Box.py
+++ Box.py
@@ -1,3 +1,9 @@
-class Box:
+class Box: 
     def __init__(self) -> None:
-        pass
+        self.image = None
+        self.number = 0
+        self.pos_x=0
+        self.pos_y=0
+        
+    
+    
