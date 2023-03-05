e1f6dfab951952116e52289aa05784c55a0baf18
ShovalShabi <shovalshabi@gmail.com>
2023-03-05 16:00:13 +0200

    added logic to prisoner.py and box.py

---
+++ Box.py
@@ -0,0 +1,15 @@
+
+
+class Box:
+
+    def __init__(self,pos_x,pos_y,number_box,next_box):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.img=None
+        self.number_box=number_box
+        self.next_box=next_box
+
+    def draw(self):
+        #drwaing to box to the screen
+        pass
+
--- Prisoner.py
+++ Prisoner.py
@@ -1,15 +1,20 @@
 from threading import Thread
+from Prisoners_Algorithm.Box import Box
+
 
 class Prisoner(Thread):
 
-    def __init__(self, num_inmate):
+    def __init__(self, num_prisoner,box):
         super().__init__()
         self.images=[]
         self.pos_x=0
         self.pos_y=0
-        self.number_inmate=num_inmate
+        self.prisoner_number=num_prisoner
+        self.trgt_box=box
+        self.visited_boxes={int:Box}
+        self.number_found=False
 
-    def walk_to_box(self):
+    def walk_to_box(self,box):
         #moving physically to box
         pass
 
@@ -17,7 +22,13 @@ class Prisoner(Thread):
         #animating the movement of the prisoner by sprite sheet
         pass
 
-    def is_reached_box(self,target_box):
-        #checking if the prisoners reached the box
-        pass
+    def is_reached_box(self):
+        if self.trgt_box.number_box == self.prisoner_number:
+            self.number_found=True
+
+    def run(self):
+        while not self.number_found:
+            self.walk_to_box()
+
+
 
