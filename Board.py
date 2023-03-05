4616460cb58b67cce93ca3193dff48113982d9ec
matanew1 <matanew1@gmail.com>
2023-03-05 12:53:58 +0200

    front

--- Board.py
+++
@@ -1,7 +0,0 @@
-class Board:
-    def __init__(self) -> None:
-        self.height=0
-        self.width=0
-        self.linked_list_boxes = []
-        self.prisoners = []
-        self.statistics = ""
--- Box.py
+++
@@ -1,9 +0,0 @@
-class Box: 
-    def __init__(self) -> None:
-        self.image = None
-        self.number = 0
-        self.pos_x=0
-        self.pos_y=0
-        
-    
-    
