944999b3734fe6f2055f5648c7077d04013f57b6
matanew1 <matanew1@gmail.com>
2023-03-07 00:07:19 +0200

    Update

--- app.py
+++ app.py
@@ -9,16 +9,17 @@ class App:
     def __init__(self) -> None:
         pygame.init()
         pygame.font.init()
-        self.font = pygame.font.SysFont('monospace', FONT_SIZE)
+        pygame.display.set_caption("Prisoners")
+        self.font = pygame.font.SysFont('monospace', FONT_SIZE, bold=True)
         self.clock = pygame.time.Clock()
         self.state = 'start'
         self.running = True
-        self.num_of_boxes = MAX_NO_BOX
+        self.num_of_boxes = 0
         self.num_of_prisoners = 1
         self.size = (screen_width, screen_height)
         self.screen = pygame.display.set_mode(self.size)
-        pygame.display.set_caption("Prisoners")
-        self.colors = SETTINGS.get('COLORS')
+        self.image = pygame.image.load(IMG_BACKGROUND)
+        self.background_image = pygame.transform.scale(self.image, (screen_width, screen_height))
         self.text_input = ""
 
     def run(self):
@@ -40,19 +41,20 @@ class App:
                         self.text_input = self.text_input[:-1]
                 else:
                     self.text_input += event.unicode
+            self.convert_input_to_num_of_box()
 
     def start_draw(self):
-        self.screen.fill(self.colors.get('BLACK'))
+        self.screen.blit(self.background_image, (0, 0))
         self.draw_boxes()
         self.draw_menu()
         # self.draw_prisoners()
 
     def draw_menu(self):
         # Draw the screen
-        self.draw_button(self.colors.get('GREEN'), 50, 800, button_width, button_height, "Start")
-        self.draw_button(self.colors.get('RED'), 200, 800, button_width, button_height, "Reset")
-        self.draw_label(self.colors.get('WHITE'), 350, 800, 'Enter number of boxes:')
-        self.draw_label(self.colors.get('WHITE'), 400, 820, self.text_input)
+        self.draw_button(GREEN, 50, 800, button_width, button_height, "Start")
+        self.draw_button(RED, 200, 800, button_width, button_height, "Reset")
+        self.draw_label(RED, 350, 800, 'Enter number of boxes: (maximum ' + str(MAX_NO_BOX) + ')')
+        self.draw_label(RED, 400, 820, self.text_input)
 
     def draw_label(self, color, x, y, text):
         text_surface = self.font.render(text, True, color)
@@ -60,19 +62,15 @@ class App:
 
     def draw_button(self, color, x, y, width, height, text):
         pygame.draw.rect(self.screen, color, (x, y, width, height))
-        text_surface = self.font.render(text, True, self.colors.get('BLACK'))
+        text_surface = self.font.render(text, True, BLACK)
         text_rect = text_surface.get_rect()
         text_rect.center = ((x + width // 2), (y + height // 2))
         self.screen.blit(text_surface, text_rect)
 
-    def draw_prisoners(self):
-        pass
-
-    def draw_box(self, col, inc):
+    def draw_box(self, box_index, inc):
         box = pygame.image.load("Resources/chest_closed.png")
-        rect = pygame.Rect(CELL_SIZE + col * CELL_SIZE, CELL_SIZE + inc * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-        pygame.draw.rect(self.screen, self.colors.get('RED'), rect, 1)
-        text_surface = self.font.render(str(col + 1 + inc * MAX_BOX_WIDTH), True, self.colors.get('YELLOW'))
+        rect = pygame.Rect(150 + box_index * CELL_SIZE, 80 + inc * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+        text_surface = self.font.render(str(box_index + 1 + inc * MAX_BOX_WIDTH), True, YELLOW)
         text_rect = text_surface.get_rect()
         text_rect.center = box.get_rect().center
         box.blit(text_surface, text_rect)
@@ -80,13 +78,21 @@ class App:
 
     def draw_boxes(self):
         if self.num_of_boxes <= MAX_BOX_WIDTH:
-            for box in range(0, self.num_of_boxes, CELL_SIZE):
-                self.draw_box(box, 0)
+            for box_index in range(self.num_of_boxes):
+                self.draw_box(box_index, 0)
         else:
             rows = int(math.floor(self.num_of_boxes / MAX_BOX_WIDTH))
             remainder = self.num_of_boxes - rows * MAX_BOX_WIDTH
             for row in range(rows):
-                for box in range(MAX_BOX_WIDTH):
-                    self.draw_box(box, row)
+                for box_index in range(MAX_BOX_WIDTH):
+                    self.draw_box(box_index, row)
             for rem in range(remainder):
                 self.draw_box(rem, rows)
+
+    def convert_input_to_num_of_box(self):
+        if self.text_input != "":
+            num = int(str(self.text_input))
+            if num <= MAX_NO_BOX:
+                self.num_of_boxes = num
+        else:
+            self.num_of_boxes = 0
--- settings.py
+++ settings.py
@@ -1,17 +1,18 @@
-# COLOR
+# Color
 
-SETTINGS = {
-    'COLORS': {
-        'GREEN': (0, 255, 0),
-        'RED': (255, 0, 0),
-        'BLACK': (0, 0, 0),
-        'PURPLE': (255, 0, 255),
-        'WHITE': (255, 255, 255),
-        'YELLOW': (255, 255, 0)
-    }
-}
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+PURPLE = (255, 0, 255)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
 
-screen_width = 1200
+# Image path
+IMG_BACKGROUND = "Resources/Lunetic_Room.jpg"
+
+
+# Screen and button
+screen_width = 1100
 screen_height = 880
 button_width = 100
 button_height = 50
@@ -19,7 +20,6 @@ button_height = 50
 FONT_SIZE = 17
 
 # BOX
-CELL_SIZE = 50
-MAX_BOX_WIDTH = 24
-MAX_BOX_HEIGHT = 24
-MAX_NO_BOX = 100
+CELL_SIZE = 80
+MAX_BOX_WIDTH = 10
+MAX_NO_BOX = 90
