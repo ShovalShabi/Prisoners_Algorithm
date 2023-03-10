869c969e00753fe6fcb3bea3886567e1e6b5bb6f
ShovalShabi <shovalshabi@gmail.com>
2023-03-10 15:11:29 +0200

    Changed logic on ModelManager and state the model connectivity part in the controller

--- Model/controller.py
+++ Model/controller.py
@@ -1,20 +1,58 @@
+from threading import Lock, Thread
 
 
 class Controller:
     """
-    A class representing the connection between the backend and the frontend, is trusted of the connecting both sub-systems messages.
+    A class representing the connection between the backend and the frontend, is trusted of the connecting both sub-systems messages.\n
     The class coordinate between the changes that are taking place in the business logic to the UI, the object
-    is also works in some functions as threaded methodology for better efficiency to the game.
+    has also some threaded functions methodology for better efficiency of the game.\n
 
-    :parameter:prisoner_num, prisoner number -> int.
-    :parameter:pos, position of the prisoner on screen-> tuple of (x,y).
-    :parameter:pace, pace of the prisoner, int.
-    :parameter:visited_boxes, all the boxes that the prisoner have opened, dictionary of {number box:value box}.
-    :parameter:all_boxes,all the boxes located on screen-> dictionary of {number box:value box}.
-    :parameter:trgt_box, the current target box of the prisoner -> BoxM object.
-    :parameter:found_number, indicator if the prisoner have found his number -> bool.
-    :parameter:updated_pos, flag the represents if the prisoner has been change position -> bool.
+    Attributes:/n
+
+    model: representation of the model manager class -> ModelManger object.\n
+    view: representation of the view manager class -> ViewManger object.\n
+    lock: mutex lock for threads -> Lock object.\n
     """
 
-    def __init__(self):
+    def __init__(self,model,view):
+        self.model=model
+        self.view=view
+        self.lock=Lock()
+        self.tasks=[]
+
+    def get_view(self):
+        return self.view
+
+    def set_view(self,view):
+        self.view=view
+
+    def get_model(self):
+        return self.model
+
+    def set_model(self, model):
+        self.model = model
+
+
+    #################### Model related methods #####################################
+    def prepare_thread_to_trgt(self,trgt_func):
         pass
+
+    def ntfy_to_view_pris_pos(self,pos:tuple):
+        pass
+
+    def ntfy_to_view_pris_need_box(self,box_num):
+        pass
+
+    def ntfy_to_view_pris_changed(self,new_pris_num):
+        pass
+
+    def ntfy_to_view_pris_need_boxes_pos(self):#will return dict of {num_box:position}
+        pass
+
+    def model_start_game(self,num_prisoners,num_round,print_specifically):
+        pass
+
+    def ntfy_to_view_pris_succeeded(self,num_succeeded):
+        pass
+
+    ###################################################################################
--- Model/model_manger.py
+++ Model/model_manger.py
@@ -25,13 +25,10 @@ class ModelManger:
     prob_handler: a probability handler object, handles with probability calculations of each round and the total success rate -> ProbabilitiesHandler object.
     """
 
-    def __init__(self,num_prisoners:int,num_rounds:int,print_specifically:bool,listener:Controller):
+    def __init__(self,listener:Controller):
         """
         Initialize ModelManger Object.\n
-        :param num_prisoners: int, the number of prisoners.
-        :param num_rounds: int, the number of rounds.
-        :param print_specifically: bool, indication if the user want full description of each prisoner and round
-        :param listener: Controller object, coordinator class between backend and frontend
+        :param listener: Controller object, coordinator class between backend and frontend.
         """
         self.dict_rounds={}  #dict of {round_num:list dependencies of boxes}
         self.dict_prisoners={}   #dict of {num_pris:prisoner}
@@ -40,33 +37,52 @@ class ModelManger:
         self.current_round = 1
         self.current_prisoner = 1
         self.succeeded=0
-        self.num_prisoners = num_prisoners
-        self.num_rounds = num_rounds
-        self.print_specifically = print_specifically
         self.prob_handler=None
 
-    ############# Listener Methods ###################################
+    ############################## MVC Methods ###################################
 
     def ntfy_pris_pos(self):
-        self.listener.ntfy_view_pris_pos(self.dict_prisoners[self.current_prisoner].get_pos())
+        self.listener.ntfy_to_view_pris_pos(self.dict_prisoners[self.current_prisoner].get_pos())
 
     def ntfy_pris_need_box(self):
-        self.listener.notify_model_need_box(self.dict_prisoners[self.current_prisoner].trgt_box.box_num)
+        self.listener.ntfy_to_view_pris_need_box(self.dict_prisoners[self.current_prisoner].trgt_box.box_num)
 
-    def ntfy_pris_changed(self):
-        self.listener.ntfy_pris_changed(self.current_prisoner.get_num())
+    def ntfy_pris_replaced(self):
+        self.listener.ntfy_to_view_pris_changed(self.current_prisoner.get_num())
 
-    def ntfy_model_need_boxes_pos(self):
-        return self.listener.ntfy_model_need_boxes_pos()  #will return dict of {num_box:position}
+    def ntfy_pris_need_boxes_pos(self):
+        return self.listener.ntfy_to_view_pris_need_boxes_pos()  #will return dict of {num_box:position}
 
-    ######################################################################
+    def model_start_game(self,num_pris,num_rounds,initial_pos,print_specifically):
+        self.run_game(num_pris=num_pris,num_rounds=num_rounds,initial_pos=initial_pos,print_specifically=print_specifically)
 
-    def init_boxes(self) -> None:
+    def ntfy_pris_succeed(self):
+        return self.listener.ntfy_to_view_pris_succeeded(self.succeeded)
+
+    ###############################################################################
+
+    def init_prisoners(self,num_pris:int,initial_pos:tuple) -> None:
         """
-        Initialization method for creating new BoxM objects according the round requirements and screen placing.\n
+        Initialization method for creating new PrisonerM objects according the round requirements.\n
+        :param num_pris: int, the number o prisoners.
+        :param initial_pos: tuple, initial position of the prisoner on screen (x,y)
         :return: None
         """
-        for index_box in range(self.num_prisoners):
+        if self.dict_prisoners:
+            self.dict_prisoners = {}
+        for index_pris in range(num_pris):
+            self.dict_prisoners[index_pris+1]=PrisonerM(num_prisoner=index_pris + 1, position=initial_pos, pace=5,
+                                                        all_boxes=self.dict_rounds[self.current_round], trgt_box=self.dict_boxes[index_pris + 1])
+
+    def init_boxes(self,num_pris) -> None:
+        """
+        Initialization method for creating new BoxM objects according the round requirements and screen placing.\n
+        :param num_pris: int, the number o prisoners.
+        :return: None
+        """
+        if self.dict_boxes:
+            self.dict_boxes = {}
+        for index_box in range(num_pris):
             box=BoxM(box_num=index_box + 1)
             self.dict_boxes[index_box+1]=box
         for box_num in self.dict_boxes.keys():  #box num starts from 1 to n+1
@@ -78,45 +94,40 @@ class ModelManger:
         Set method for ll boxes position, the method requests the controller to hand over the box locations on the screen.\n
         :return: None
         """
-        positions = self.ntfy_model_need_boxes_pos()
+        positions = self.ntfy_pris_need_boxes_pos()
         for box_num in self.dict_boxes.keys():
             self.dict_boxes[box_num].set_pos = positions[box_num]
 
-    def init_prisoners(self) -> None:
-        """
-        Initialization method for creating new PrisonerM objects according the round requirements.\n
-        :return: None
-        """
-        for index_pris in range(self.num_prisoners):
-            self.dict_prisoners[index_pris+1]=PrisonerM(num_prisoner=index_pris + 1, position=(0, 0), pace=5, all_boxes=self.dict_rounds[self.current_round], trgt_box=self.dict_boxes[index_pris + 1])
-
-    def run_game(self) -> None:
+    def run_game(self,num_pris,num_rounds,initial_pos,print_specifically) -> None:
         """
         The actual method that responsible for the game functionality.\n
-        At first the function run the ProbabilityManager calculations and let the priosners move by its requirements.\n
+        At first the function run the ProbabilityManager calculations and let the prisoners move by its requirements.\n
+        :param num_pris: int, the number o prisoners.
+        :param num_rounds: int, the number of rounds.
+        :param initial_pos: tuple, the initial position of the prisoner on screen (x,y)
+        :param print_specifically: bool, indication if the user want full description of each prisoner and round
         :return: None
         """
-        self.prob_handler=ProbabilitiesHandler(num_prisoners=self.num_prisoners,num_rounds=self.num_rounds,print_specifically=self.print_specifically)
+        self.prob_handler=ProbabilitiesHandler(num_prisoners=num_pris,num_rounds=num_rounds,print_specifically=print_specifically)
         self.dict_rounds=self.prob_handler.run_probabilities()
-        while self.current_round < self.num_rounds:
+        self.current_round = 1
+        self.init_boxes(num_pris=num_pris)
+        self.init_prisoners(num_pris=num_pris,initial_pos=initial_pos)
+        while self.current_round < num_rounds:
 
-            if self.current_prisoner > self.num_prisoners:
+            if self.current_prisoner > num_pris:
                 self.current_prisoner=1
                 self.current_round+=1
-                if self.current_round > self.num_rounds:
+                self.succeeded=0
+                if self.current_round > num_rounds:
                     return
                 else:
                     self.current_round+=1
-
-            self.init_boxes()
-            self.init_prisoners()
+                    self.init_boxes(num_pris=num_pris)
+                    self.init_prisoners(num_pris=num_pris, initial_pos=initial_pos)
 
             while self.dict_prisoners[self.current_prisoner].is_still_searching():
-                pass
                 self.ntfy_pris_need_box()
                 self.dict_prisoners[self.current_prisoner].move_to_box()
                 self.ntfy_pris_pos()
             self.current_prisoner+=1
-
-
-
