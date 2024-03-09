5bbf6aee5bf076b6e748e3056fd52139820d5c3a
ShovalShabi <shovalshabi@gmail.com>
2024-03-09 16:37:20 +0200

    Added cProfile for detecting code bottlenecks

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Model.controller import Controller
 from Model.modelmanager import ModelManger
 from View.viewmanager import ViewManager
+import cProfile
 
 if __name__ == '__main__':
     model = ModelManger()
@@ -10,4 +11,5 @@ if __name__ == '__main__':
     model.set_listener(controller)
     view.set_listener(controller)
 
-    view.run()
+    # view.run()
+    cProfile.run('view.run()')  # For running analytics for finding code bottlenecks
